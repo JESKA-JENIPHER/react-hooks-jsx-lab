3afa94aa1ce3a16c5c98c4b77d8cdd043865c322
JESKA JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-07-12 15:38:08 +0300

    add components

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">About
+    <h2>About Me</h2>
+    <p>JSX practice lab</p>
+    <img src = "https://i.imgur.com/mV8PQxj.gif" alt = "I made this" />
+  </div>
+  )
+  
 }
 
 export default About;
